1bc8ee59d73b3a5d9e9cecc6fa561095cc156852
Hwan Goh <Hwan.Goh@gmail.com>
2019-10-19 16:28:42 -0500

    ADMM coded?

--- Codes/ADMM_methods.py
+++ Codes/ADMM_methods.py
@@ -52,49 +52,33 @@ def ADMM_penalty_term(NN, pen, z_weights, z_biases, lagrange_weights, lagrange_b
 ###############################################################################
 def update_z_and_lagrange_multiplier(sess, NN, alpha, pen, z_weights, z_biases, lagrange_weights, lagrange_biases):   
     for l in range(0, len(NN.weights)):  
-        sess.run(tf.assign(z_weights[l], soft_threshold_weights(NN, lagrange_weights, lagrange_biases, alpha, pen)))
-        sess.run(tf.assign(z_biases[l], soft_threshold_biases(NN, lagrange_weights, lagrange_biases, alpha, pen)))
+        sess.run(tf.assign(z_weights[l], soft_threshold_weights(NN, l, lagrange_weights, lagrange_biases, alpha, pen)))
+        sess.run(tf.assign(z_biases[l], soft_threshold_biases(NN, l, lagrange_weights, lagrange_biases, alpha, pen)))
         sess.run(tf.assign(lagrange_weights[l], pen*(NN.weights[l] - z_weights[l])))
         sess.run(tf.assign(lagrange_biases[l], pen*(NN.biases[l] - z_biases[l])))
       
 ###############################################################################
 #                       Soft Thresholding Operator                            #
 ###############################################################################   
-def soft_threshold_weights(NN, lagrange_weights, lagrange_biases, alpha, pen):
-    weights_val = []
-    for l in range(0, len(NN.weights)):
-        val = NN.weights[l] + lagrange_weights[l]/pen
-        weights_val.append(val)
-        
-        # annoying digital logic workaround to implement conditional.
-        # construct vectors of 1's and 0's that we can multiply
-        # by the proper value and sum together
-        cond1 = tf.where(tf.greater(weights_val, alpha/pen), tf.ones((NN.layers[l], NN.layers[l + 1])), tf.zeros((NN.layers[l], NN.layers[l + 1])))
-        cond3 = tf.where(tf.less(weights_val, -1.0*alpha/pen), tf.ones((NN.layers[l], NN.layers[l + 1])), tf.zeros((NN.layers[l], NN.layers[l + 1])))
-        # cond2 is not needed since the complement of the intersection
-        # of (cond1 and cond3) is cond2 and already assigned to 0
-        
-        z_weights = cond1*(weights_val[l] - alpha/pen) + cond3*(weights_val[l] + alpha/pen)
+def soft_threshold_weights(NN, l, lagrange_weights, lagrange_biases, alpha, pen):
+    weights_val = NN.weights[l] + lagrange_weights[l]/pen
     
-    return z_weights
+    cond1 = tf.where(tf.greater(weights_val, alpha/pen), tf.ones((NN.layers[l], NN.layers[l + 1])), tf.zeros((NN.layers[l], NN.layers[l + 1])))
+    cond3 = tf.where(tf.less(weights_val, -1.0*alpha/pen), tf.ones((NN.layers[l], NN.layers[l + 1])), tf.zeros((NN.layers[l], NN.layers[l + 1])))
+    
+    new_z_weights = cond1*(weights_val - alpha/pen) + cond3*(weights_val + alpha/pen)
+    
+    return new_z_weights
 
-def soft_threshold_biases(NN, lagrange_weights, lagrange_biases, alpha, pen):
-    biases_val = []
-    for l in range(0, len(NN.weights)):
-        val = NN.biases[l] + lagrange_biases[l]/pen
-        biases_val.append(val)
-        
-        # annoying digital logic workaround to implement conditional.
-        # construct vectors of 1's and 0's that we can multiply
-        # by the proper value and sum together
-        cond1 = tf.where(tf.greater(biases_val, alpha/pen), tf.ones((1, NN.layers[l + 1])), tf.zeros((1, NN.layers[l + 1])))
-        cond3 = tf.where(tf.less(biases_val, -1.0*alpha/pen), tf.ones((1, NN.layers[l + 1])), tf.zeros((1, NN.layers[l + 1])))
-        # cond2 is not needed since the complement of the intersection
-        # of (cond1 and cond3) is cond2 and already assigned to 0
-        
-        z_biases = cond1*(biases_val[l] - alpha/pen) + cond3*(biases_val[l] + alpha/pen)
+def soft_threshold_biases(NN, l, lagrange_weights, lagrange_biases, alpha, pen):
+    biases_val = NN.biases[l] + lagrange_biases[l]/pen
     
-    return z_biases
+    cond1 = tf.where(tf.greater(biases_val, alpha/pen), tf.ones((1, NN.layers[l + 1])), tf.zeros((1, NN.layers[l + 1])))
+    cond3 = tf.where(tf.less(biases_val, -1.0*alpha/pen), tf.ones((1, NN.layers[l + 1])), tf.zeros((1, NN.layers[l + 1])))
+
+    new_z_biases = cond1*(biases_val - alpha/pen) + cond3*(biases_val + alpha/pen)
+    
+    return new_z_biases
 
     
 
