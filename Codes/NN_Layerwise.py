1d4a9d2cf9457da5ff3dda5458d0545196c61939
Hwan Goh <Hwan.Goh@gmail.com>
2019-10-20 16:43:25 -0500

    small update

--- Codes/NN_Layerwise.py
+++ Codes/NN_Layerwise.py
@@ -48,44 +48,37 @@ class Layerwise:
         #   Extending Architecture   #
         ##############################   
         if weight_list_counter > 0: 
-            # Load pre-trained hidden layer weights
-            for l in range(0, weight_list_counter):
-                df_trained_weights = pd.read_csv(savefilepath + "_W" + str(l+1) + '.csv')
-                df_trained_biases = pd.read_csv(savefilepath + "_b" + str(l+1) + '.csv')
-                restored_W = df_trained_weights.at[0, "W"+str(l+1)]
-                restored_b = df_trained_biases.at[0, "b"+str(l+1)]
-                W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = restored_W, trainable = False)
-                b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = restored_b, trainable = False)                                  
-                self.weights.append(W)
-                self.biases.append(b)
-            # Create list entry for new layer
-            self.weights.append("new_layer")
-            self.biases.append("new_layer")
-            # Load output layer weights
-            l = weight_list_counter + 1
-            df_trained_weights = pd.read_csv(savefilepath + "_Woutput" + '.csv')
-            df_trained_biases = pd.read_csv(savefilepath + "_boutput" + '.csv')
-            restored_W = df_trained_weights.at[0, "Woutput"]
-            restored_b = df_trained_biases.at[0, "boutput"]
-            W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = restored_W)
-            b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = restored_b)                                  
-            self.weights.append(W) # add a copy of the output weights which also extends list of weights
-            self.biases.append(b) # add a copy of the output biases which also extends list of biases
-            
-            pdb.set_trace()
-            
-            # Initialize new hidden layer as 0           
             with tf.variable_scope("NN_layerwise") as scope: 
-                l = weight_list_counter
-                W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = tf.constant_initializer(0))
-                b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = tf.constant_initializer(0))                                  
-                tf.summary.histogram("weights" + str(l+1), W)
-                tf.summary.histogram("biases" + str(l+1), b)
-                self.weights[l] = W # replace list entry for last hidden layer of weights; was previously equal to output weights
-                self.biases[l] = b  # replace list entry for last hidden layer of biases; was previously equal to output biases
-        
-        # Ensures train.Saver only saves the weights and biases                
-        self.saver_NN_layerwise = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "NN_layerwise")      
+                # Load pre-trained weights and biases
+                for l in range(0, weight_list_counter):
+                    df_trained_weights = pd.read_csv(savefilepath + "_W" + str(l+1) + '.csv')
+                    df_trained_biases = pd.read_csv(savefilepath + "_b" + str(l+1) + '.csv')
+                    restored_W = df_trained_weights.at[0, "W"+str(l+1)]
+                    restored_b = df_trained_biases.at[0, "b"+str(l+1)]
+                    W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = restored_W, trainable = False)
+                    b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = restored_b, trainable = False)                                  
+                    self.weights.append(W)
+                    self.biases.append(b)
+                
+                # Initialize new hidden layer weights and biases as 0           
+                    l = weight_list_counter
+                    W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = tf.constant_initializer(0))
+                    b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = tf.constant_initializer(0))                                  
+                    tf.summary.histogram("weights" + str(l+1), W)
+                    tf.summary.histogram("biases" + str(l+1), b)
+                    self.weights.append(W)
+                    self.biases.append(b)
+                    
+                # Load pre-trained output layer weights and biases. Note these can be trained again
+                    l = weight_list_counter + 1
+                    df_trained_weights = pd.read_csv(savefilepath + "_Woutput" + '.csv')
+                    df_trained_biases = pd.read_csv(savefilepath + "_boutput" + '.csv')
+                    restored_W = df_trained_weights.at[0, "Woutput"]
+                    restored_b = df_trained_biases.at[0, "boutput"]
+                    W = tf.get_variable("W" + str(l+1), dtype = tf.float32, shape = [self.layers[l], self.layers[l + 1]], initializer = restored_W)
+                    b = tf.get_variable("b" + str(l+1), dtype = tf.float32, shape = [1, self.layers[l + 1]], initializer = restored_b)                                  
+                    self.weights.append(W)
+                    self.biases.append(b)
         
 ###############################################################################
 #                           Network Propagation                               #
