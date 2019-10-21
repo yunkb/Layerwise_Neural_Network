ae2968e61c5c6bc0a009708b90d622e9f75bcb1e
HwanGoh <Hwan.Goh@gmail.com>
2019-10-21 17:45:44 -0500

    small update

--- Codes/Training_Driver_LayerwiseNN_ADMM.py
+++ Codes/Training_Driver_LayerwiseNN_ADMM.py
@@ -41,7 +41,7 @@ class HyperParameters:
     penalty           = 1
     node_TOL          = 1e-3
     error_TOL         = 1e-3
-    batch_size        = 1000
+    batch_size        = 100
     num_epochs        = 5
     gpu               = '1'
     
