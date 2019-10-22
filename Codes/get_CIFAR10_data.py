2eff5181d950986985dfcb4139af26b5f3139289
Hwan Goh <Hwan.Goh@gmail.com>
2019-10-21 22:13:55 -0500

    small update

--- Codes/get_CIFAR10_data.py
+++ Codes/get_CIFAR10_data.py
@@ -26,7 +26,7 @@ def load_CIFAR10_data(flatten_data_flag):
     
     return num_training_data, num_testing_data, img_size, num_channels, label_dimensions, class_names, data_train, labels_train, data_test, labels_test
 
-def get_CIFAR10_batch(data_train, labels_train, batch_size, flatten_data_flag):
+def get_CIFAR10_batch(data_train, labels_train, batch_size):
     # Number of images in the training-set.
     num_images = len(data_train)
 
