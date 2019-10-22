4ba63f2fc356ec2bc11a1c77e198cb9dfd64eec6
Hwan Goh <Hwan.Goh@gmail.com>
2019-10-21 22:11:54 -0500

    small update

--- Codes/Training_Driver_LayerwiseNN_ADMM.py
+++ Codes/Training_Driver_LayerwiseNN_ADMM.py
@@ -94,7 +94,7 @@ def trainer(hyper_p, run_options):
     if run_options.data_MNIST == 1:
         mnist, num_training_data, num_testing_data, data_dimensions, label_dimensions, data_test, labels_test = load_MNIST_data()
     if run_options.data_CIFAR10 == 1:    
-        num_training_data, num_testing_data, img_size, num_channels, label_dimensions, class_names, data_train, labels_train, data_test, labels_test = load_CIFAR10_data()
+        num_training_data, num_testing_data, img_size, num_channels, label_dimensions, class_names, data_train, labels_train, data_test, labels_test = load_CIFAR10_data(flatten_data_flag = 1)
         data_dimensions = img_size*img_size*num_channels
         
     #=== Iteration Objects ===#
@@ -181,7 +181,7 @@ def trainer(hyper_p, run_options):
                     if run_options.data_MNIST == 1:
                         data_train_batch, labels_train_batch = get_MNIST_batch(mnist, hyper_p.batch_size)
                     if run_options.data_CIFAR10 == 1: 
-                        data_train_batch, labels_train_batch = get_CIFAR10_batch(data_train, labels_train, hyper_p.batch_size, 1)
+                        data_train_batch, labels_train_batch = get_CIFAR10_batch(data_train, labels_train, hyper_p.batch_size)
                     #loss_value, _, s = sess.run([loss, optimizer_Adam_op, summ], feed_dict = {NN.data_tf: data_train_batch, NN.labels_tf: labels_train_batch}) 
                     #writer.add_summary(s, epoch)
                     loss_value, _ = sess.run([loss, optimizer_Adam_op], feed_dict = {NN.data_tf: data_train_batch, NN.labels_tf: labels_train_batch}) 
--- Codes/get_CIFAR10_data.py
+++ Codes/get_CIFAR10_data.py
@@ -10,7 +10,7 @@ from CIFAR10_Hvass import cifar10
 from CIFAR10_Hvass.cifar10 import img_size, num_channels, num_classes
 import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
 
-def load_CIFAR10_data():    
+def load_CIFAR10_data(flatten_data_flag):    
     cifar10.maybe_download_and_extract()    
     class_names = cifar10.load_class_names()
     data_train, cls_train, labels_train = cifar10.load_training_data()
@@ -19,6 +19,10 @@ def load_CIFAR10_data():
     num_training_data = len(data_train)
     num_testing_data = len(data_test)
     label_dimensions = num_classes 
+    
+    if flatten_data_flag == 1:
+        data_train = data_train.reshape((num_training_data, img_size*img_size*num_channels))
+        data_test = data_train.reshape((num_testing_data, img_size*img_size*num_channels))
     
     return num_training_data, num_testing_data, img_size, num_channels, label_dimensions, class_names, data_train, labels_train, data_test, labels_test
 
@@ -34,8 +38,5 @@ def get_CIFAR10_batch(data_train, labels_train, batch_size, flatten_data_flag):
     # Use the random index to select random images and labels.
     data_train_batch = data_train[idx, :, :, :]
     labels_train_batch = labels_train[idx, :]
-    
-    if flatten_data_flag == 1:
-        data_train_batch = data_train_batch.reshape((batch_size, img_size*img_size*num_channels))
 
     return data_train_batch, labels_train_batch
