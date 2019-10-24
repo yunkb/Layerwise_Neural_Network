e3eb7d1775de4f64adccc39bb12a0b81f6ceb7f2
Co Tran <cotran2@utexas.edu>
2019-10-24 18:17:19 -0500

    fixed some errors

--- Codes_Co/Keras_scripts/utils.py
+++ Codes_Co/Keras_scripts/utils.py
@@ -18,8 +18,8 @@ def get_data(dataset):
         (x_train, y_train), (x_test, y_test) = datasets.cifar100.load_data()
         x_train, x_test = x_train / 255.0, x_test / 255.0
     x_train = tf.cast(x_train,tf.float32)
-    x_test = tf.cast(x_test, tf.int32)
-    y_train = tf.cast(y_train, tf.float32)
+    x_test = tf.cast(x_test, tf.float32)
+    y_train = tf.cast(y_train, tf.int32)
     y_test = tf.cast(y_test, tf.int32)
     return x_train,y_train,x_test,y_test
 
