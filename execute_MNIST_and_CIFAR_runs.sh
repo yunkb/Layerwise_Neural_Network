ec7e9d8213a63d36825954b54372a8fd818d1c15
Hwan Goh <Hwan.Goh@gmail.com>
2019-10-26 14:39:21 -0500

    small update

--- execute_MNIST_and_CIFAR_runs.sh
+++ execute_MNIST_and_CIFAR_runs.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/env bash
 
 screen -S test -d -m
-screen -S test . /workspace/hwan/anaconda3/etc/profile.d/conda.sh && source activate tf_gpu
+screen -S test -X source activate tf_gpu
 screen -S test -X chdir Codes
 screen -S test -X ./Training_Driver_NNLayerwise_L2.py
