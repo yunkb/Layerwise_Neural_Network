4cbc75c0d8f99a620aecac5b7d256e53bd73d5e8
HwanGoh <Hwan.Goh@gmail.com>
2019-11-01 17:50:13 -0500

    small update

--- Codes_TF2/Training_Driver_CNNLayerwise.py
+++ Codes_TF2/Training_Driver_CNNLayerwise.py
@@ -30,7 +30,7 @@ class HyperParameters:
     error_TOL         = 1e-2
     batch_size        = 1000
     num_epochs        = 60
-    gpu               = '0'
+    gpu               = '2'
     
 class RunOptions:
     def __init__(self, hyper_p):    
