475dd3f76ba3f59f0f8b77b916e31f20cbc77875
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-02 12:56:07 -0500

    small update

--- Codes_TF2/Utilities/optimize_ADMM_layerwise.py
+++ Codes_TF2/Utilities/optimize_ADMM_layerwise.py
@@ -122,7 +122,7 @@ def optimize_ADMM(hyper_p, run_options, NN, data_and_labels_train, data_and_labe
             print('Time per Epoch: %.2f\n' %(elapsed_time_epoch))
             print('Training Set: Loss: %.3e, Accuracy: %.3f' %(loss_train_batch_average.result(), accuracy_train_batch_average.result()))
             print('Validation Set: Loss: %.3e, Accuracy: %.3f\n' %(loss_val_batch_average.result(), accuracy_val_batch_average.result()))
-            print('Previous Layer Relative # of 0s\n' %(relative_number_zeros))
+            print('Previous Layer Relative # of 0s: %.7f\n' %(relative_number_zeros))
             start_time_epoch = time.time()   
         
         ########################################################
