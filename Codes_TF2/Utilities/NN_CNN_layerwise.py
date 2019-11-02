now plots rel_zeros and fixed up computation of rel_zeros

Codes_TF2/Utilities/NN_CNN_layerwise.py
@@ -123,16 +123,16 @@ class CNNLayerwise(tf.keras.Model):
         trained_weights = self.hidden_layers_list[-1].get_weights()
         sparsified_weights = []
         for w in trained_weights:
-            bool_mask = (w > threshold).astype(int)
+            bool_mask = (abs(w) > threshold).astype(int)
             sparsified_weights.append(w*bool_mask)
         self.hidden_layers_list[-1].set_weights(sparsified_weights)
         
         total_number_of_zeros = 0
         total_number_of_elements = 0
-        for i in range(0, len(trained_weights)):
-            total_number_of_zeros += np.count_nonzero(trained_weights[i]==0)
-            total_number_of_elements += trained_weights[i].flatten().shape[0]
-        relative_number_zeros = total_number_of_zeros/total_number_of_elements
+        for i in range(0, len(sparsified_weights)):
+            total_number_of_zeros += np.count_nonzero(sparsified_weights[i]==0)
+            total_number_of_elements += sparsified_weights[i].flatten().shape[0]
+        relative_number_zeros = np.float64(total_number_of_zeros/total_number_of_elements)
         
         return relative_number_zeros
     

Codes_TF2/Utilities/plot_and_save_figures_layerwise.py
@@ -60,15 +60,16 @@ def plot_and_save_figures(hyper_p, run_options):
     try:
         df_rel_zeros = pd.read_csv(run_options.NN_savefile_name + "_relzeros" + '.csv')
         rel_zeros_array = df_rel_zeros.to_numpy()
+        rel_zeros_array = rel_zeros_array.flatten()
     except:
         print('No relative number of zeros .csv file!')
     rel_zeros_array_exists = 'rel_zeros_array' in locals() or 'rel_zeros_array' in globals()
     if rel_zeros_array_exists:
         fig_accuracy = plt.figure()
-        x_axis = np.linspace(2,hyper_p.max_hidden_layers, hyper_p.max_hidden_layers)
+        x_axis = np.linspace(2, hyper_p.max_hidden_layers-1, hyper_p.max_hidden_layers-2, endpoint = True)
         plt.plot(x_axis, rel_zeros_array, label = 'relative # of 0s')
-        plt.title('Rel_#_0s: ' + run_options.filename)
-        plt.xlabel('Layers')
+        plt.title('Rel # of 0s: ' + run_options.filename)
+        plt.xlabel('Layer Number')
         plt.ylabel('Number of Zeros')
         plt.legend()
         figures_savefile_name = run_options.figures_savefile_directory + '/' + 'rel_num_zeros_' + run_options.filename + '.png'