665a5c1e53cbf54b324d277466845debd20bafbc
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-01 20:41:37 -0500

    small update

---
+++ Codes_TF2/plotting_results_CNNLayerwise.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 21:23:46 2019
+
+@author: hwan
+"""
+
+from Utilities.plot_and_save_figures_layerwise import plot_and_save_figures
+from decimal import Decimal # for filenames
+import os
+import sys
+
+import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
+
+###############################################################################
+#                       HyperParameters and RunOptions                        #
+###############################################################################
+class HyperParameters:
+    max_hidden_layers = 8 # For this architecture, need at least 2. One for the mapping to the feature space, one as a trainable hidden layer. EXCLUDES MAPPING BACK TO DATA SPACE
+    filter_size       = 3
+    num_filters       = 6
+    regularization    = 1
+    node_TOL          = 1e-3
+    error_TOL         = 1e-2
+    batch_size        = 1000
+    num_epochs        = 60
+    gpu               = '2'
+    
+class RunOptions:
+    def __init__(self, hyper_p):    
+        #=== Use L_1 Regularization ===#
+        self.use_L1 = 1
+        
+        #=== Choose Data Set ===#
+        data_MNIST = 0
+        data_CIFAR10 = 1 
+        data_CIFAR100 = 0
+        
+        #=== Random Seed ===#
+        self.random_seed = 1234
+        
+        #=== Use LBFGS Optimizer ===#
+        self.use_LBFGS = 0
+        
+        #=== Setting Filename ===# 
+        self.NN_type = 'CNN'
+        if data_MNIST == 1:
+            self.dataset = 'MNIST'
+        if data_CIFAR10 == 1:
+            self.dataset = 'CIFAR10'
+        if data_CIFAR100 == 1:
+            self.dataset = 'CIFAR100'
+        if hyper_p.regularization >= 1:
+            hyper_p.regularization = int(hyper_p.regularization)
+            regularization_string = str(hyper_p.regularization)
+        else:
+            regularization_string = str(hyper_p.regularization)
+            regularization_string = 'pt' + regularization_string[2:]                        
+        node_TOL_string = str('%.2e' %Decimal(hyper_p.node_TOL))
+        node_TOL_string = node_TOL_string[-1]
+        error_TOL_string = str('%.2e' %Decimal(hyper_p.error_TOL))
+        error_TOL_string = error_TOL_string[-1]
+        
+        if self.use_L1 == 0:
+            self.filename = self.dataset + '_' + self.NN_type + '_mhl%d_fs%d_nf%d_eTOL%s_b%d_e%d' %(hyper_p.max_hidden_layers, hyper_p.filter_size, hyper_p.num_filters, error_TOL_string, hyper_p.batch_size, hyper_p.num_epochs)
+        else:
+            self.filename = self.dataset + '_' + self.NN_type + '_L1_mhl%d_fs%d_nf%d_r%s_nTOL%s_eTOL%s_b%d_e%d' %(hyper_p.max_hidden_layers, hyper_p.filter_size, hyper_p.num_filters, regularization_string, node_TOL_string, error_TOL_string, hyper_p.batch_size, hyper_p.num_epochs)
+
+        #=== Saving neural network ===#
+        self.NN_savefile_directory = '../Trained_NNs/' + self.filename # Since we save the parameters for each layer separately, we need to create a new folder for each model
+        self.NN_savefile_name = self.NN_savefile_directory + '/' + self.filename # The file path and name for the saved parameters
+
+        #=== Saving Figures ===#
+        self.figures_savefile_directory = '../Figures/' + self.filename
+
+        #=== Creating Directories ===#
+        if not os.path.exists(self.NN_savefile_directory):
+            os.makedirs(self.NN_savefile_directory)
+        if not os.path.exists(self.figures_savefile_directory):
+            os.makedirs(self.figures_savefile_directory)
+            
+###############################################################################
+#                                  Driver                                     #
+###############################################################################
+if __name__ == "__main__":
+    
+    #=== Hyperparameters ===#    
+    hyper_p = HyperParameters()
+    
+    if len(sys.argv) > 1:
+        hyper_p.max_hidden_layers = int(sys.argv[1])
+        hyper_p.filter_size       = int(sys.argv[2])
+        hyper_p.num_filters       = int(sys.argv[3])
+        hyper_p.regularization    = float(sys.argv[4])
+        hyper_p.node_TOL          = float(sys.argv[5])
+        hyper_p.error_TOL         = float(sys.argv[6])
+        hyper_p.batch_size        = int(sys.argv[7])
+        hyper_p.num_epochs        = int(sys.argv[8])
+        hyper_p.gpu               = int(sys.argv[9])
+            
+    #=== Set run options ===#         
+    run_options = RunOptions(hyper_p)
+    
+    #=== Plot and save figures ===#
+    plot_and_save_figures(hyper_p, run_options)
