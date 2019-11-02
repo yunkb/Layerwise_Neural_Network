28548c45e3392f9517cd998e89b4f61a8fb457b3
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-02 11:13:55 -0500

    small update

--- Codes_TF2/Utilities/optimize_ADMM_layerwise.py
+++ Codes_TF2/Utilities/optimize_ADMM_layerwise.py
@@ -82,10 +82,10 @@ def optimize_ADMM(hyper_p, run_options, NN, data_and_labels_train, data_and_labe
                         NN.summary()
                         z, lagrange = initialize_z_and_lagrange_multiplier(NN.get_weights()) 
                     ADMM_penalty = update_ADMM_penalty_terms(hyper_p.penalty, NN.weights, z, lagrange)
-                    print(str(ADMM_penalty.numpy()))
                     loss_train_batch = data_loss(output, labels_train, label_dimensions) + ADMM_penalty
                     gradients = tape.gradient(loss_train_batch, NN.trainable_variables)
                     optimizer.apply_gradients(zip(gradients, NN.trainable_variables))
+                    print('Loss Value: %.3f, ADMM_penalty: %.3f' %(loss_train_batch, ADMM_penalty.numpy()))
                     elapsed_time_batch = time.time() - start_time_batch
                     if batch_num  == 0:
                         print('Time per Batch: %.2f' %(elapsed_time_batch))
