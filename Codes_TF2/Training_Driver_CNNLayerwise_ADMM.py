6135f0dcceca22145f0ae5f1eced0636cf30fd95
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-02 11:56:53 -0500

    small update

--- Codes_TF2/Training_Driver_CNNLayerwise_ADMM.py
+++ Codes_TF2/Training_Driver_CNNLayerwise_ADMM.py
@@ -29,7 +29,7 @@ class HyperParameters:
     error_TOL         = 1e-2
     batch_size        = 1000
     num_epochs        = 20
-    gpu               = '0'
+    gpu               = '1'
     
 class RunOptions:
     def __init__(self, hyper_p):        
