c3e771b7ae7fd15dae02f769123c245c64ce0735
HwanGoh <Hwan.Goh@gmail.com>
2019-11-04 13:37:13 -0600

    small update

--- Codes_TF2/Training_Driver_FCLayerwise.py
+++ Codes_TF2/Training_Driver_FCLayerwise.py
@@ -36,7 +36,7 @@ class HyperParameters:
     node_TOL          = 1e-4
     error_TOL         = 1e-4
     batch_size        = 1000
-    num_epochs        = 30
+    num_epochs        = 5
     gpu               = '3'
     
 class RunOptions:
