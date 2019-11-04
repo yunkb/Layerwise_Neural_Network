3666f9a491089c0d33c02e84cb97203ef08add54
HwanGoh <Hwan.Goh@gmail.com>
2019-11-04 11:23:40 -0600

    small update

--- Codes_TF2/Training_Driver_FCLayerwise.py
+++ Codes_TF2/Training_Driver_FCLayerwise.py
@@ -32,8 +32,8 @@ import sys
 class HyperParameters:
     max_hidden_layers = 8 # For this architecture, need at least 2. One for the mapping to the feature space, one as a trainable hidden layer. EXCLUDES MAPPING BACK TO DATA SPACE
     num_hidden_nodes  = 1000
-    regularization    = 0.000
-    node_TOL          = 1e-4
+    regularization    = 0.001
+    node_TOL          = 1e-5
     error_TOL         = 1e-4
     batch_size        = 1000
     num_epochs        = 30
