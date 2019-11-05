0b7a1c5a9fc69ea6a893315137547d2cdf726fe4
HwanGoh <Hwan.Goh@gmail.com>
2019-11-05 10:24:08 -0600

    small update

--- Codes_TF2/Training_Driver_CNNLayerwise.py
+++ Codes_TF2/Training_Driver_CNNLayerwise.py
@@ -30,12 +30,12 @@ class HyperParameters:
     error_TOL         = 1e-4
     batch_size        = 3000
     num_epochs        = 30
-    gpu               = '3'
+    gpu               = '2'
     
 class RunOptions:
     def __init__(self, hyper_p):    
         #=== Use L_1 Regularization ===#
-        self.use_L1 = 0
+        self.use_L1 = 1
         
         #=== Choose Data Set ===#
         data_MNIST = 0
