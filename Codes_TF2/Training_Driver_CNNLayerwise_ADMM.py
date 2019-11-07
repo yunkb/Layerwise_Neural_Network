6ae5ae190f7db526b929f6dee7aeea0b80cb09ff
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-06 21:09:49 -0600

    small update

--- Codes_TF2/Training_Driver_CNNLayerwise_ADMM.py
+++ Codes_TF2/Training_Driver_CNNLayerwise_ADMM.py
@@ -22,7 +22,7 @@ import sys
 class HyperParameters:
     max_hidden_layers = 8 # For this architecture, need at least 2. One for the mapping to the feature space, one as a trainable hidden layer. EXCLUDES MAPPING BACK TO DATA SPACE
     filter_size       = 3
-    num_filters       = 128
+    num_filters       = 64
     regularization    = 0.001
     penalty           = 5
     node_TOL          = 1e-4
