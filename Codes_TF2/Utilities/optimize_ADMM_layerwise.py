8ae9f2239d12c94c7e2685e8b537bfe4f12626c2
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-10 13:28:10 -0600

    small update

--- Codes_TF2/Utilities/optimize_ADMM_layerwise.py
+++ Codes_TF2/Utilities/optimize_ADMM_layerwise.py
@@ -175,7 +175,7 @@ def optimize_ADMM(hyper_p, run_options, NN, data_and_labels_train, data_and_labe
             elapsed_time_epoch = time.time() - start_time_epoch
             print('Time per Epoch: %.2f\n' %(elapsed_time_epoch))
             print('Training Set: Data Loss: %.3e, Loss: %.3e, Accuracy: %.3f' %(data_loss_train_batch_average.result(), loss_train_batch_average.result(), accuracy_train_batch_average.result()))
-            print('Validation Set: Data Loss: %.3e, Loss: %.3e, Accuracy: %.3f\n' %(data_loss_val_batch_average.result(), loss_val_batch_average.result(), accuracy_val_batch_average.result()))
+            print('Validation Set: Data Loss: %.3e, Loss: %.3e, Accuracy: %.3f' %(data_loss_val_batch_average.result(), loss_val_batch_average.result(), accuracy_val_batch_average.result()))
             print('Test Set: Data_Loss: %.3e, Loss: %.3e, Accuracy: %.3f\n' %(data_loss_test_batch_average.result(), loss_test_batch_average.result(), accuracy_test_batch_average.result()))
             print('Previous Layer Relative # of 0s: %.7f\n' %(relative_number_zeros))
             start_time_epoch = time.time() 
