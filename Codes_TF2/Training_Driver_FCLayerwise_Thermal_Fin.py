923b33cb34faec7e7ed37d04d6382d0bc7660710
Hwan Goh <Hwan.Goh@gmail.com>
2019-11-18 21:48:37 -0600

    small update

--- Codes_TF2/Training_Driver_FCLayerwise_Thermal_Fin.py
+++ Codes_TF2/Training_Driver_FCLayerwise_Thermal_Fin.py
@@ -41,7 +41,7 @@ import sys
 class HyperParameters:
     data_type         = 'full'
     max_hidden_layers = 8 # For this architecture, need at least 2. One for the mapping to the feature space, one as a trainable hidden layer. EXCLUDES MAPPING BACK TO DATA SPACE
-    num_hidden_nodes  = 500
+    num_hidden_nodes  = 10
     regularization    = 0.0002
     node_TOL          = 1e-4
     error_TOL         = 1e-4
